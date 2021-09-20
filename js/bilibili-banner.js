a9f1834d2f073816ef3de8ec0425550328557ac2
Lanqilu <885240677@qq.com>
2021-09-20 14:08:51 +0800

    Site updated: 2021-09-20 14:08:26

--- js/bilibili-banner.js
+++ js/bilibili-banner.js
@@ -1,1 +1,117 @@
-"use strict";function _classCallCheck(t,e){if(!(t instanceof e))throw new TypeError("Cannot call a class as a function")}function _defineProperties(t,e){for(var r=0;r<e.length;r++){var n=e[r];n.enumerable=n.enumerable||!1,n.configurable=!0,"value"in n&&(n.writable=!0),Object.defineProperty(t,n.key,n)}}function _createClass(t,e,r){return e&&_defineProperties(t.prototype,e),r&&_defineProperties(t,r),t}function _toConsumableArray(t){return _arrayWithoutHoles(t)||_iterableToArray(t)||_unsupportedIterableToArray(t)||_nonIterableSpread()}function _nonIterableSpread(){throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.")}function _unsupportedIterableToArray(t,e){if(t){if("string"==typeof t)return _arrayLikeToArray(t,e);var r=Object.prototype.toString.call(t).slice(8,-1);return"Map"===(r="Object"===r&&t.constructor?t.constructor.name:r)||"Set"===r?Array.from(t):"Arguments"===r||/^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(r)?_arrayLikeToArray(t,e):void 0}}function _iterableToArray(t){if("undefined"!=typeof Symbol&&null!=t[Symbol.iterator]||null!=t["@@iterator"])return Array.from(t)}function _arrayWithoutHoles(t){if(Array.isArray(t))return _arrayLikeToArray(t)}function _arrayLikeToArray(t,e){(null==e||e>t.length)&&(e=t.length);for(var r=0,n=new Array(e);r<e;r++)n[r]=t[r];return n}var intX,mb=document.getElementById("springBanner"),imgs=document.querySelectorAll(".layer img");console.log(imgs);var o_imgs_styles=(imgs=_toConsumableArray(imgs)).map(function(t){t=window.getComputedStyle(t,null).getPropertyValue("transform");return"transform: ".concat(t)}),o_imgs_opacitys=imgs.map(function(t){window.getComputedStyle(t,null).getPropertyValue("opacity");return"opacity: ".concat(0)});function getTranslateVal(t){t.indexOf("translate")}mb.onmouseenter=function(t){t=t.x;intX=t},mb.onmousemove=function(t){var a=t.x;imgs.forEach(function(t,e){var r=t.dataset.moveMultiple,n=(intX-a)/Number(r),i=o_imgs_styles[e].split(","),r=Number(i[4]);i.splice(4,1,String(r+n));r=i.join(",");t.dataset.isOpacity&&(n=o_imgs_opacitys[e].split(":"),Number(n[1]),i=window.screen.width/2,e=Number(t.dataset.isOpacity)?intX-a:a-intX,n.splice(1,1,e/i),n=n.join(":"),r+="; ".concat(n)),t.setAttribute("style",r)})},mb.onmouseleave=function(){imgs.forEach(function(t,e){t.setAttribute("style",o_imgs_styles[e])})};var c=document.getElementById("canvas"),ctx=c.getContext("2d"),_w=c.width,_h=c.height,scales=[.03,.04,.05,.06,.07,.08],slen=scales.length-1,speedX=2,speedY=3,flower=function(){function t(){_classCallCheck(this,t),this.x=Math.random()*_w,this.y=0,this.speedX=0,this.speedY=0,this.scale=0,this.width=0,this.height=0,this.img=null}return _createClass(t,[{key:"init",value:function(){this.img=new Image;var t=5<10*Math.random()?1:0,e=Math.floor(Math.random()*slen);this.speedX=Math.random()*speedX,this.speedY=Math.random()*speedY,this.scale=scales[e],this.width=this.height=250*this.scale,this.img.src=t?"https://cdn.jsdelivr.net/gh/halo-blog/cdn-blog-img-a@main/BilibiliBannerSpring/flow1.png":"https://cdn.jsdelivr.net/gh/halo-blog/cdn-blog-img-a@main/BilibiliBannerSpring/flow2.png"}},{key:"draw",value:function(t){var e=this;this.img.onload=function(){t.drawImage(e.img,e.x,e.y,e.width,e.height)}}},{key:"move",value:function(t){this.x=this.x+this.speedX>=_w?Math.random()*_w:this.x+this.speedX,this.y=this.y>=_h?0:this.y+this.speedY,t.drawImage(this.img,this.x,this.y,this.width,this.height)}}]),t}(),flowers=[];function start(){for(var t=0;t<50;t++){var e=new flower;e.init(),e.draw(ctx),flowers.push(e)}}function move(){ctx.clearRect(0,0,_w,_h);for(var t=0;t<flowers.length;t++)flowers[t].move(ctx);window.requestAnimationFrame(move)}start(),move();
+// 鼠标移动
+const mb = document.getElementById('springBanner')
+let intX
+let imgs = document.querySelectorAll('.layer img')
+console.log(imgs)
+imgs = [...imgs]
+
+let o_imgs_styles = imgs.map(el => {
+    let s = window.getComputedStyle(el, null).getPropertyValue('transform')
+    return `transform: ${s}`
+})
+
+let o_imgs_opacitys = imgs.map(el => {
+    let o = window.getComputedStyle(el, null).getPropertyValue('opacity')
+    return `opacity: ${0}`
+})
+
+function getTranslateVal(str) {
+    let s = 'translate', e = ')'
+    let si = str.indexOf(s)
+    let ei
+}
+
+mb.onmouseenter = function ({ x }) {
+    intX = x
+}
+mb.onmousemove = function ({ x }) {
+    imgs.forEach((el, i) => {
+        const dm = el.dataset.moveMultiple
+        const dis = (intX - x) / Number(dm)
+        const transform_arr = o_imgs_styles[i].split(',')
+        let translate_x = Number(transform_arr[4])
+        let new_translate_x = translate_x + dis
+        transform_arr.splice(4, 1, String(new_translate_x))
+        let transform_str = transform_arr.join(',')
+
+        if (el.dataset.isOpacity) {
+            const o_arr = o_imgs_opacitys[i].split(':')
+            const o_value = Number(o_arr[1])
+            let href_w = window.screen.width / 2
+
+            const o_dis = Number(el.dataset.isOpacity) ? intX - x : x - intX
+
+            let new_o_value = o_dis / href_w
+            o_arr.splice(1, 1, new_o_value)
+            let o_str = o_arr.join(':')
+            transform_str += `; ${o_str}`
+        }
+
+        el.setAttribute('style', transform_str)
+    })
+}
+mb.onmouseleave = function () {
+    imgs.forEach((el, i) => {
+        el.setAttribute('style', o_imgs_styles[i])
+    })
+}
+
+// 樱花飘落canvas 
+const c = document.getElementById('canvas')
+const ctx = c.getContext('2d')
+const _w = c.width, _h = c.height;
+const scales = [0.03, 0.04, 0.05, 0.06, 0.07, 0.08];
+const slen = scales.length - 1;
+const speedX = 2, speedY = 3;
+class flower {
+    constructor() {
+        this.x = Math.random() * _w;
+        this.y = 0;
+        this.speedX = 0;
+        this.speedY = 0;
+        this.scale = 0;
+        this.width = 0;
+        this.height = 0;
+        this.img = null
+    }
+    init() {
+        this.img = new Image()
+        const f = Math.random() * 10 > 5 ? 1 : 0
+        const si = Math.floor(Math.random() * slen)
+        this.speedX = Math.random() * speedX
+        this.speedY = Math.random() * speedY
+        this.scale = scales[si]
+        this.width = this.height = 250 * this.scale
+        const src = f ? "https://cdn.jsdelivr.net/gh/halo-blog/cdn-blog-img-a@main/BilibiliBannerSpring/flow1.png" : 'https://cdn.jsdelivr.net/gh/halo-blog/cdn-blog-img-a@main/BilibiliBannerSpring/flow2.png';
+        this.img.src = src
+    }
+    draw(ctx) {
+        this.img.onload = () => {
+            ctx.drawImage(this.img, this.x, this.y, this.width, this.height)
+        }
+    }
+    move(ctx) {
+        this.x = this.x + this.speedX >= _w ? Math.random() * _w : this.x + this.speedX;
+        this.y = this.y >= _h ? 0 : this.y + this.speedY;
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height)
+    }
+}
+const flowers = []
+function start() {
+    const max = 50;
+    for (let i = 0; i < max; i++) {
+        const f = new flower()
+        f.init()
+        f.draw(ctx)
+        flowers.push(f)
+    }
+}
+function move() {
+    ctx.clearRect(0, 0, _w, _h)
+    for (let i = 0; i < flowers.length; i++) {
+        flowers[i].move(ctx)
+    }
+    window.requestAnimationFrame(move)
+}
+start()
+move()
--- js/butterfly.js
+++ js/butterfly.js
@@ -1,1 +1,12 @@
-"use strict";
+// console.log("Hello");
+
+// window.onload = function(){
+//     var _iframe = document.getElementById('iframe').contentWindow.document.getElementById('tabs')   //get iframe下的id
+//     console.log("Hello1");
+//     console.log(_iframe);
+//     _iframe.style.fontSize=16+'px';
+//     console.log("Hello2");
+// }
+
+// console.log("Hello3");
+
